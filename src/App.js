9a0695c854d788178a932353eb14c32e550a47f7
Davyd <orbivanlove@gmail.com>
2023-12-20 12:06:33 -0500

    add link on my github in header

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
       <header className="App-header">
         <h1>Hello World!</h1>
         <p>This is my Jammming App</p>
+        <a href="https://github.com/dkhatkin">This is my GitHub</a>
       </header>
     </div>
   );
