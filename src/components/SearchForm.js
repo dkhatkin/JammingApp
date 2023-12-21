b4ff293612c606caa82aec601769d5eba692fbca
Davyd <orbivanlove@gmail.com>
2023-12-21 10:22:42 -0500

    Add Logic to SearchForm.js

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SearchForm.css";
 
 function SearchForm() {
+  const [userinput, setUserinput] = useState("");
+  function handleChange({ target }) {
+    setUserinput(target.value);
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <form className="SearchForm">
-      <label className="SearchLabel" for="searchbar">Enter A Song Title</label>
+    <form className="SearchForm" onSubmit={handleSubmit}>
+      <label className="SearchLabel" for="searchbar">
+        Enter A Song Title
+      </label>
       <input
         className="SearchBar"
         id="searchbar"
         type="text"
         placeholder="Thunderstruck - AC/DC"
+        value={userinput}
+        onChange={handleChange}
       />
       <input className="SubmitButton" type="submit" value="SEARCH" />
     </form>
